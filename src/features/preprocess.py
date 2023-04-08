b7ec1b3852a0babb72cf3adb2c839e0f9ce8ed51
Karl Haraldsson <karl.haraldsson@gmail.com>
2023-04-08 15:50:39 -0500

    BOW SVM model testing

--- src/features/preprocess.py
+++ src/features/preprocess.py
@@ -3,11 +3,14 @@ import string
 import pandas as pd
 import numpy as np
 
+from sklearn.feature_extraction.text import CountVectorizer
+
 
 class Data:
     def __init__(self, raw_df, name):
         self.raw_df = raw_df
         self.name = name
+        self.vectorizer = None
         self.text = None
         self.label = None
 
@@ -15,21 +18,21 @@ class Data:
     def from_csv(cls, train_path, name):
         return cls(pd.read_csv(train_path), name)
 
-    def process(self, text_name, target_name):
+    def process(self, text_name, target_name, vectorizor=None):
         # TODO Document
         # Convert target into array of labels
         self.label = self._process_target(target_name)
 
         # Preprocess text data
-        self.text = self._process_text(text_name)
+        self.text = self._process_text(text_name, vectorizor)
 
     def _process_target(self, target_name):
         return self.raw_df[target_name]
 
-    def _process_text(self, text_name):
+    def _process_text(self, text_name, vectorizor=None):
         # TODO Document
         text_clean = np.array([self._clean_text(element) for element in self.raw_df[text_name]])
-        text_vector = self._vectorize_text(text_clean)
+        text_vector = self._vectorize_text(text_clean, vectorizor)
         text_embeddings = self._apply_embeddings(text_vector)
         return text_embeddings
 
@@ -47,9 +50,29 @@ class Data:
         text = re.sub(r"\s+", ' ', text).strip()
         return text
 
-    def _vectorize_text(self, text):
-        # TODO Build this out. Should be num instances x vocabulary shape numpy array.
-        return text
+    def _vectorize_text(self, text, vectorizer=None):
+        # TODO Build this out. Should be num instances x vocabulary shape numpy array.d
+        #   Probably doesn't make sense to use CountVectorizer, given that we want some flexibility with embeddings.
+
+        # On the text set, use an existing vectorizer for the training data.
+        if vectorizer is None:
+            if self.name == 'train':
+                vectorizer = CountVectorizer()
+            else:
+                raise ValueError("vectorizer cannot be None when self.name is not 'train'")
+
+        # Fit and transform the data for training data
+        if self.name == 'train':
+            text_matrix = vectorizer.fit_transform(text)
+
+            # Save Vectorizer Object for use with dev/test data
+            self.vectorizer = vectorizer
+
+        # Only transform the data for dev or test data
+        elif self.name == 'dev' or self.name == 'test':
+            text_matrix = vectorizer.transform(text)
+
+        return text_matrix
 
     def _apply_embeddings(self, text):
         # TODO Apply pretrained embeddings
--- src/main.py
+++ src/main.py
@@ -7,21 +7,47 @@ TBD
 
 import os
 import pandas as pd
-from pathlib import Path
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 from src.features import preprocess
+from src.modeling import classifier
 
 from argparse import ArgumentParser
 
 
 def main(training_data_file, development_data_file, test_data_file, predictions_file, results_file):
-    train = preprocess.Data.from_csv(training_data_file, name='train')
-    print(train.raw_df.shape)
-    train.process(text_name='text', target_name='HS')
-    print(train.label.shape)
-    print(train.text.shape)
+    # Load Data from CSV and store as preprocess.Data object
+    data_train = preprocess.Data.from_csv(training_data_file, name='train')
+    data_dev = preprocess.Data.from_csv(development_data_file, name='dev')
 
+    # Preprocess Data
+    data_train.process(text_name='text', target_name='HS')
+    data_dev.process(text_name='text', target_name='HS', vectorizor=data_train.vectorizer)
 
+    # Train Model
+    # parameter_grid = {'C': [0.1, 1, 10], 'kernel': ['linear', 'rbf', 'sigmoid']}
+    parameter_grid = {'C': [0.1], 'kernel': ['linear']}
+    clf = classifier.Model(parameter_grid)
+    clf.fit(data_train.text, data_train.label, cv_folds=3)
+
+    # Predict on Dev Set
+    pred_labels = clf.predict(data_dev.text)
+
+    # Evaluate classifier
+    accuracy = accuracy_score(data_dev.label, pred_labels)
+    precision = precision_score(data_dev.label, pred_labels, average='binary')
+    recall = recall_score(data_dev.label, pred_labels, average='binary')
+    f1 = f1_score(data_dev.label, pred_labels, average='binary')
+
+    print("Accuracy: {:.2f}".format(accuracy))
+    print("Precision: {:.2f}".format(precision))
+    print("Recall: {:.2f}".format(recall))
+    print("F1-score: {:.2f}".format(f1))
+
+    # Output Predictions
+
+    # Output Evaluation Results
 
 
 if __name__ == "__main__":
