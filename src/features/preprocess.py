dc6dfa670fe37eee7ed8522ffacbc6c835cb98f8
Nora Miao <norah.myo@gmail.com>
2023-04-11 15:35:40 -0700

    Add data preprocessing functions

--- src/features/preprocess.py
+++ src/features/preprocess.py
@@ -2,14 +2,17 @@ import re
 import string
 import pandas as pd
 import numpy as np
-
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import TweetTokenizer
+from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import CountVectorizer
 
 
 class Data:
     def __init__(self, raw_df, name):
         self.raw_df = raw_df
-        self.name = name
+        self.name = name  # train, dev, or test
         self.vectorizer = None
         self.text = None
         self.label = None
@@ -19,22 +22,43 @@ class Data:
         return cls(pd.read_csv(train_path), name)
 
     def process(self, text_name, target_name, vectorizor=None):
-        # TODO Document
         # Convert target into array of labels
         self.label = self._process_target(target_name)
-
         # Preprocess text data
         self.text = self._process_text(text_name, vectorizor)
 
     def _process_target(self, target_name):
-        return self.raw_df[target_name]
+        return np.array(self.raw_df[target_name])
 
     def _process_text(self, text_name, vectorizor=None):
-        # TODO Document
-        text_clean = np.array([self._clean_text(element) for element in self.raw_df[text_name]])
+        text_clean = np.array(
+            [self._clean_text(element) for element in self.raw_df[text_name]]
+        )
         text_vector = self._vectorize_text(text_clean, vectorizor)
         text_embeddings = self._apply_embeddings(text_vector)
         return text_embeddings
+
+    def _tokenize(self, text):
+        tweet_tokenizer = TweetTokenizer()
+        tokens = tweet_tokenizer.tokenize(text)
+        return tokens
+
+    def _lemmatize(self, tokens):
+        lemmatizer = WordNetLemmatizer()
+        lemmatized_tokens = [lemmatizer.lemmatize(token) for token in tokens]
+        return lemmatized_tokens
+
+    def _remove_stopwords(self, tokens):
+        stop_words = set(stopwords.words("english"))
+        return [token for token in tokens if not token in stop_words]
+
+    def _extract_hashtags(self, text):
+        hashtags = re.findall(r"#[A-Za-z0-9_]+", text)
+        hashtag_tokens_combined = []
+        for hashtag in hashtags:
+            hashtag_tokens = re.findall(r"[A-Z]+[a-z]*", hashtag[1:])
+            hashtag_tokens_combined.extend(hashtag_tokens)
+        return [token.lower() for token in hashtag_tokens_combined]
 
     def _clean_text(self, text):
         """
@@ -42,13 +66,19 @@ class Data:
         TODO: Determine how much of this is actually necessary.
             This is pretty standard preprocessing, but not necessarily how we want to treat tweets.
         """
-        text = str(text)
-        text = text.lower()
-        text = re.sub(r'/', ' ', text)
-        text = re.sub(r'\[.*?\]', '', text)
-        text = re.sub(r'[%s]' % re.escape(string.punctuation), '', text)
-        text = re.sub(r"\s+", ' ', text).strip()
-        return text
+        hashtag_tokens = self._extract_hashtags(text)
+        text = str(text).lower()  # Convert to lowercase
+        text = re.sub(r"https?://\S+", " ", text)  # Remove URLs
+        text = re.sub(r"[#@][A-Za-z0-9_]+", " ", text)  # Remove hashtags and mentions
+        text = re.sub(
+            r"[\W\d]+", " ", text
+        ).strip()  # Remove non-word characters, numbers, and extra whitespaces
+        tokens = self._tokenize(text)
+        lemmatized_tokens = self._lemmatize(tokens)
+        filtered_tokens = self._remove_stopwords(lemmatized_tokens)
+        filtered_tokens.extend(hashtag_tokens)
+        clean_text = " ".join(filtered_tokens)
+        return clean_text
 
     def _vectorize_text(self, text, vectorizer=None):
         # TODO Build this out. Should be num instances x vocabulary shape numpy array.d
@@ -56,20 +86,22 @@ class Data:
 
         # On the text set, use an existing vectorizer for the training data.
         if vectorizer is None:
-            if self.name == 'train':
+            if self.name == "train":
                 vectorizer = CountVectorizer()
             else:
-                raise ValueError("vectorizer cannot be None when self.name is not 'train'")
+                raise ValueError(
+                    "vectorizer cannot be None when self.name is not 'train'"
+                )
 
         # Fit and transform the data for training data
-        if self.name == 'train':
+        if self.name == "train":
             text_matrix = vectorizer.fit_transform(text)
 
             # Save Vectorizer Object for use with dev/test data
             self.vectorizer = vectorizer
 
         # Only transform the data for dev or test data
-        elif self.name == 'dev' or self.name == 'test':
+        elif self.name == "dev" or self.name == "test":
             text_matrix = vectorizer.transform(text)
 
         return text_matrix
--- src/main.py
+++ src/main.py
@@ -10,24 +10,32 @@ import pandas as pd
 
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-from src.features import preprocess
-from src.modeling import classifier
+from features import preprocess
+from modeling import classifier
 
 from argparse import ArgumentParser
 
 
-def main(training_data_file, development_data_file, test_data_file, predictions_file, results_file):
+def main(
+    training_data_file,
+    development_data_file,
+    test_data_file,
+    predictions_file,
+    results_file,
+):
     # Load Data from CSV and store as preprocess.Data object
-    data_train = preprocess.Data.from_csv(training_data_file, name='train')
-    data_dev = preprocess.Data.from_csv(development_data_file, name='dev')
+    data_train = preprocess.Data.from_csv(training_data_file, name="train")
+    data_dev = preprocess.Data.from_csv(development_data_file, name="dev")
 
     # Preprocess Data
-    data_train.process(text_name='text', target_name='HS')
-    data_dev.process(text_name='text', target_name='HS', vectorizor=data_train.vectorizer)
+    data_train.process(text_name="text", target_name="HS")
+    data_dev.process(
+        text_name="text", target_name="HS", vectorizor=data_train.vectorizer
+    )
 
     # Train Model
     # parameter_grid = {'C': [0.1, 1, 10], 'kernel': ['linear', 'rbf', 'sigmoid']}
-    parameter_grid = {'C': [0.1], 'kernel': ['linear']}
+    parameter_grid = {"C": [0.1], "kernel": ["linear"]}
     clf = classifier.Model(parameter_grid)
     clf.fit(data_train.text, data_train.label, cv_folds=3)
 
@@ -36,9 +44,9 @@ def main(training_data_file, development_data_file, test_data_file, predictions_
 
     # Evaluate classifier
     accuracy = accuracy_score(data_dev.label, pred_labels)
-    precision = precision_score(data_dev.label, pred_labels, average='binary')
-    recall = recall_score(data_dev.label, pred_labels, average='binary')
-    f1 = f1_score(data_dev.label, pred_labels, average='binary')
+    precision = precision_score(data_dev.label, pred_labels, average="binary")
+    recall = recall_score(data_dev.label, pred_labels, average="binary")
+    f1 = f1_score(data_dev.label, pred_labels, average="binary")
 
     print("Accuracy: {:.2f}".format(accuracy))
     print("Precision: {:.2f}".format(precision))
@@ -61,15 +69,15 @@ if __name__ == "__main__":
     # arg_parser.add_argument('--predictions_file', type=str, required=True)
     # arg_parser.add_argument('--results_file', type=str, required=True)
     # args = arg_parser.parse_args()
-    training_data_file = '../data/train/en/hateval2019_en_train.csv'
-    development_data_file = '../data/dev/en/hateval2019_en_dev.csv'
-    test_data_file = '../data/test/en/hateval2019_en_test.csv'
-    predictions_file = '../outputs/pred_en.txt'
-    results_file = '../outputs/res.txt'
+    training_data_file = "../data/train/en/hateval2019_en_train.csv"
+    development_data_file = "../data/dev/en/hateval2019_en_dev.csv"
+    test_data_file = "../data/test/en/hateval2019_en_test.csv"
+    predictions_file = "../outputs/pred_en.txt"
+    results_file = "../outputs/res.txt"
     main(
-        training_data_file=training_data_file
-        , development_data_file=development_data_file
-        , test_data_file=test_data_file
-        , predictions_file=predictions_file
-        , results_file=results_file
+        training_data_file=training_data_file,
+        development_data_file=development_data_file,
+        test_data_file=test_data_file,
+        predictions_file=predictions_file,
+        results_file=results_file,
     )
