4a8df2666c83e546e84ef3b03092c3834d9f6fb4
Nora Miao <norah.myo@gmail.com>
2023-04-16 19:35:56 -0700

    Fix minor formatting issues

--- src/main.py
+++ src/main.py
@@ -8,13 +8,7 @@ import os
 import time
 import pandas as pd
 
-from sklearn.metrics import (
-    accuracy_score,
-    precision_score,
-    recall_score,
-    f1_score,
-    classification_report,
-)
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
 
 from features import preprocess, extract_features
 from modeling import classifier
@@ -23,20 +17,20 @@ from argparse import ArgumentParser
 
 
 def output_lines(lines, path):
-    with open(path, "w+") as f:
+    with open(path, 'w+') as f:
         for line in lines:
-            f.write(str(line) + "\n")
+            f.write(str(line) + '\n')
 
 
 def main(
-    training_data_file,
-    development_data_file,
-    test_data_file,
-    predictions_file,
-    results_file,
-    model_file,
-    empath,
-    train,
+        training_data_file,
+        development_data_file,
+        test_data_file,
+        predictions_file,
+        results_file,
+        model_file,
+        empath,
+        train
 ):
     # Load Data from CSV and store as preprocess.Data object
     data_train = preprocess.Data.from_csv(training_data_file, name="train")
@@ -45,7 +39,7 @@ def main(
     # Preprocess Data
     data_train.process(text_name="text", target_name="HS")
     data_dev.process(text_name="text", target_name="HS")
-
+    
     # Extract Features from Data
     train_vector = extract_features.Vector(name="train", text=data_train.text)
     dev_vector = extract_features.Vector(name="dev", text=data_dev.text)
@@ -54,10 +48,10 @@ def main(
 
     if train:
         # Train Model
-        parameter_grid = {"C": [0.1, 1, 10], "kernel": ["linear", "rbf", "sigmoid"]}
+        parameter_grid = {'C': [0.1, 1, 10], 'kernel': ['linear', 'rbf', 'sigmoid']}
         # parameter_grid = {"C": [0.1], "kernel": ["linear"]}
         clf = classifier.Model(parameter_grid)
-        clf.fit(train_vector.vector, data_train.label, cv_folds=5, algorithm="SVM")
+        clf.fit(train_vector.vector, data_train.label, cv_folds=5, algorithm='SVM')
 
         # Save Model
         clf.save_model(model_file)
@@ -76,17 +70,17 @@ def main(
     recall = recall_score(data_dev.label, pred_labels, average="binary")
     f1 = f1_score(data_dev.label, pred_labels, average="binary")
 
-    print(f"accuracy = {accuracy:.2f}")
-    print(f"precision = {precision:.2f}")
-    print(f"recall = {recall:.2f}")
-    print(f"f1 = {f1:.2f}")
+    print(f'accuracy = {accuracy:.2f}')
+    print(f'precision = {precision:.2f}')
+    print(f'recall = {recall:.2f}')
+    print(f'f1 = {f1:.2f}')
 
     report = classification_report(data_dev.label, pred_labels)
     print(report)
 
     # Output Evaluation Results
     # TODO Mohamed to replace eval script provided by shared task
-    with open(results_file, "w") as f:
+    with open(results_file, 'w') as f:
         f.write(report)
 
 
@@ -119,5 +113,5 @@ if __name__ == "__main__":
         results_file=results_file,
         model_file=model_file,
         empath=empath,
-        train=train,
+        train=train
     )
