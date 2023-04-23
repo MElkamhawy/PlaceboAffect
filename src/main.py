7eded1bff4c457cba2409949b8283f9d5c69c1be
Karl Haraldsson <karl.haraldsson@gmail.com>
2023-04-23 17:08:04 -0500

    add pandas import

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import random
 import sys
 from argparse import ArgumentParser
 import numpy as np
+import pandas as pd
 
 import nltk
 from sklearn.metrics import accuracy_score, classification_report
