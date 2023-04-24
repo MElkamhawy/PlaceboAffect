77910076984698f0a3004106c41b2122bf792a18
Mohamed Elkamhawy <melkamhawy@github.com>
2023-04-23 17:44:17 -0700

    Updating Paths for SVM Baseline.

--- src/train.sh
+++ src/train.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate PlaceboAffect
-python3 main.py --mode train --baseline --train-data "../data/train/en/hateval2019_en_train.csv" --test-data "../data/dev/en/hateval2019_en_dev.csv" --result "../results/D2_scores.out" --prediction "../outputs/D2/pred_en_svm.txt"
+python3 main.py --mode train --baseline --train-data "../data/train/en/hateval2019_en_train.csv" --test-data "../data/dev/en/hateval2019_en_dev.csv" --result "../results/D2_scores.out" --prediction "../outputs/D2/pred_en_svm_baseline.txt"
 
