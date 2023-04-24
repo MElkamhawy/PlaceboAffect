d5fa5bcc3dc8219371088f2e4c55faba92548db7
Mohamed Elkamhawy <melkamhawy@github.com>
2023-04-23 17:09:08 -0700

    Adding create env script

---
+++ src/create_env.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+source ~/anaconda3/etc/profile.d/conda.sh
+conda env create -f requirements.yml
+conda activate PlaceboAffect
+python -m nltk.downloader all
+python -m spacy download en_core_web_sm
