dd9550e90bdbc4c741a185eaee089daffa6040e8
Mohamed Elkamhawy <melkamhawy@github.com>
2023-04-23 17:31:22 -0700

    Adding environment activation

--- src/test.sh
+++ src/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 source ~/anaconda3/etc/profile.d/conda.sh
-
+conda activate PlaceboAffect
 python3 main.py --mode test --baseline --test-data "../data/dev/en/hateval2019_en_dev.csv" --result "../results/D2_scores.out" --prediction "../outputs/D2/pred_en_svm_baseline.txt" --model "../models/D2/svm_en_baseline.pkl"
 
