c02412d4d9b1205ead23c1c6d8befff0206f2235
Nora Miao <norah.myo@gmail.com>
2023-05-02 11:57:01 -0700

    Remove unnecessary files

--- src/test.sh
+++ src/test.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-python3 main.py --mode test --baseline --test-data "../data/dev/en/hateval2019_en_dev.csv" --result "../results/D2_scores.out" --prediction "../outputs/D2/pred_en_svm_baseline.txt" --model "../models/D2/svm_en_baseline.pkl"
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate PlaceboAffect
+
+python3 src/main.py --mode test --baseline --test-data $1 --result $2 --prediction $3 --model $4 --train-data $5
--- src/test_embd.sh
+++ src/test_embd.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate PlaceboAffect
 python3 main.py --mode test --train-data "../data/train/en/hateval2019_en_train.csv" --test-data "../data/dev/en/hateval2019_en_dev.csv" --result "../results/D2_scores_embd.out" --prediction "../outputs/D2/pred_en_svm_embd.txt" --model "../models/D2/svm_en_embd.pkl"
-
