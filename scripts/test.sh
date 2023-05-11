6090b7103646a34a11f17d5dececea2200e33536
Karl Haraldsson <karl.haraldsson@gmail.com>
2023-05-11 10:53:04 -0500

    add sub directories, update scripts for D4

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate PlaceboAffect
-
-python3 src/main.py --mode test --test-data $1 --result $2 --prediction $3 --model $4 --train-data $5 --config $6
+python3 ../src/main.py --mode test --train-data "../data/train/en/hateval2019_en_train.csv" \
+--dev-data "../data/dev/en/hateval2019_en_dev.csv" \
+--test-data "../data/test/en/hateval2019_en_test.csv" \
+--result "../results/D4/D4_scores.out" \
+--prediction "../outputs/D4/pred_baseline.txt" \
+--model "../models/D4/baseline.pkl" \
+--config "../src/configs/baseline.yaml"
--- scripts/test_beta.sh
+++ scripts/test_beta.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate PlaceboAffect
-python3 ../src/main.py --mode test --train-data "../data/train/en/hateval2019_en_train.csv" --test-data "../data/dev/en/hateval2019_en_dev.csv" --result "../results/D3_scores_beta.out" --prediction "../outputs/D3/pred_beta.txt" --model "../models/D3/beta.pkl" --config "../src/configs/beta.yaml"
+python3 ../src/main.py --mode test --train-data "../data/train/en/hateval2019_en_train.csv" \
+--dev-data "../data/dev/en/hateval2019_en_dev.csv" \
+--test-data "../data/test/en/hateval2019_en_test.csv" \
+--result "../results/D4/D4_scores_beta.out" \
+--prediction "../outputs/D4/pred_beta.txt" \
+--model "../models/D4/beta.pkl" \
+--config "../src/configs/beta.yaml"
