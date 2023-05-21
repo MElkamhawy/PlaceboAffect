ff7d32c7a085903227fa0771d3ad3b473922a8cc
Karl Haraldsson <karl.haraldsson@gmail.com>
2023-05-21 17:14:21 -0500

    point D4_primary.cmd at the new test.sh

--- D4_primary.cmd
+++ D4_primary.cmd
@@ -1,8 +1,7 @@
 universe            = vanilla
-executable          = scripts/test_primary.sh
-myfiles             = ./
+executable          = scripts/test.sh
 getenv              = true
 error               = error.err
 log                 = logs.log
-arguments           = $(myfiles)/data/dev/en/hateval2019_en_test.csv $(myfiles)/results/D3_scores.out $(myfiles)/outputs/D3/pred_baseline.txt $(myfiles)/models/D3/baseline.pkl $(myfiles)/data/train/en/hateval2019_en_train.csv $(myfiles)/src/configs/baseline.yaml $(myfiles)/data/dev/en/hateval2019_en_dev.csv
+arguments           = baseline
 queue
