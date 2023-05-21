aa821397f6eb5872e894d5474eb68df327b745e9
Karl Haraldsson <karl.haraldsson@gmail.com>
2023-05-21 17:13:10 -0500

    rm .sh and cmd that runs all test/train .sh in scripts/

--- D4_run_all.cmd
+++
@@ -1,7 +0,0 @@
-universe            = vanilla
-executable          = scripts/condor_run_all_scripts.sh
-getenv              = true
-error               = error.err
-log                 = logs.log
-arguments           = baseline
-queue
