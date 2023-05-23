c887ab100df35d59b02b94c32b370bd3e0001aed
Nora Miao <norah.myo@gmail.com>
2023-05-23 10:36:21 -0700

    Update model path and scripts

--- scripts/train_adapt.sh
+++ scripts/train_adapt.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 SCRIPT_PATH="$(realpath "$0")"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
-"$SCRIPT_DIR/model_runner.sh" -m train -t adaptation -s baseline
+"$SCRIPT_DIR/model_runner.sh" -m train -t adaptation -s "$1"
--- scripts/train_alpha.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-SCRIPT_PATH="$(realpath "$0")"
-SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
-"$SCRIPT_DIR/model_runner.sh" -m train -t primary -s alpha
