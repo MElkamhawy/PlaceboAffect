05ad12d38606ab1ede952a881033c3a9754323bb
Karl Haraldsson <karl.haraldsson@gmail.com>
2023-05-27 13:33:05 -0500

    switch condor files to delta and alpha models

--- D4_primary.cmd
+++ D4_primary.cmd
@@ -3,5 +3,5 @@ executable          = scripts/test.sh
 getenv              = true
 error               = error.err
 log                 = logs.log
-arguments           = baseline
+arguments           = alpha
 queue
